979124a7116439d575b281e71d34ca5917b78166
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-11 10:15:24 -0600

    Adding template

---
+++ pex1.c
@@ -0,0 +1,23 @@
+/** pex1.c
+ * =============================================================
+ * Name: Your Name
+ * Section:  Your Section
+ * Project:  FILL IN
+ * Purpose:  FILL IN
+ * Documentation Statement:  FILL IN
+ * ============================================================= */
+
+// Include the Curses library
+// Don't forget to install it on your computer!
+#include <ncurses.h>
+	
+
+
+// Put Your Drawing Functions Here
+
+
+
+// Put Your Main Function Here
+
+
+
