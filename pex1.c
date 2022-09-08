aa58a460a4bd2b5bb33a2b812cc8aeb507563370
TaterTat24 <C24Benjamin.Tat@afacademy.af.edu>
2022-09-08 14:16:39 -0600

    Lab 1 Progress

--- pex1.c
+++ pex1.c
@@ -1,15 +1,14 @@
 /** pex1.c
  * =============================================================
- * Name: Your Name
- * Section:  Your Section
- * Project:  FILL IN
- * Purpose:  FILL IN
- * Documentation Statement:  FILL IN
+ * Name: Benjamin Tat
+ * Section: Capt McGraw - T6
+ * Project: PEX 1
+ * Purpose: Your Name in Pixels
+ * Documentation Statement: None
  * ============================================================= */
 
 // Include the Curses library
-	
-
+#include <ncurses.h>
 
 // Put Your Drawing Functions Here
 // Remember that at least one must use parameters
@@ -17,6 +16,21 @@
 
 
 // Put Your Main Function Here
+int main(int argc, char const *argv[]) {
+    initscr(); // Start curses mode
 
+    start_color();
+    
 
+    move(40, 70); //move cursor near bottom of the window
+    printw("Created with Curses By C2C Benjamin Tat"); // display a message 
 
+    // prevent applicaiton from ending until keystoke
+    move(23,10); // move cursor near bottom of the window
+    printw("press a key to continue"); // display a message
+    getch(); // wait for keystroke
+    
+    endwin(); // End curses mode
+
+    return 0;
+}
